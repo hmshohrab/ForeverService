c5314a6b83059e8a5d29936e88c2c41c65564b62
sohorab <shohrab.swl@gmail.com>
2023-09-13 18:03:36 +0600

    Initial Commit

---
+++ app/src/main/java/com/hmshohrab/foreverservice/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.hmshohrab.foreverservice
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.hmshohrab.foreverservice.R
+import com.hmshohrab.foreverservice.ForeverService.Companion.NOTIFICATION_PERMISSION_CODE
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        val stopButton = findViewById<Button>(R.id.stopButton)
+
+        val packageName = applicationContext.packageName
+        val intent = Intent()
+        intent.action = Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
+        intent.data = Uri.parse("package:$packageName")
+        startActivity(intent)
+
+        startButton.setOnClickListener {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                checkAndRequestPermission()
+            }
+
+            val intent = Intent(this, ForeverService::class.java)
+            startService(intent)
+        }
+
+        stopButton.setOnClickListener {
+            val intent = Intent(this, ForeverService::class.java)
+            intent.action = ForeverService.ACTION_STOP_SERVICE
+            startService(intent)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun checkAndRequestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), NOTIFICATION_PERMISSION_CODE)
+        }
+    }
+}
